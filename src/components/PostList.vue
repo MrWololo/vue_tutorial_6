7a373e9022e565719895c4bf8f092115419dc359
MrWololo <martin_pedrosa@hotmail.com>
2021-03-07 16:07:52 -0300

    commit

---
+++ src/components/PostList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post-list"></div>
+  <div v-for="post in posts" :key="post.id">
+    <SinglePost :post="post" />
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from "vue";
+import SinglePost from "./SinglePost.vue";
+
+export default {
+  props: ["posts"],
+  components: { SinglePost },
+  setup() {
+    onMounted(() => console.log("mounted lol"));
+    onUnmounted(() => console.log("unmounted lol"));
+    onUpdated(() => console.log("updating lol"));
+  },
+};
+</script>
+
+<style>
+</style>
